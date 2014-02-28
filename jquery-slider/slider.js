ab5cad57b00ef71c4481add11cf7f45838077b6c
catsjungle <catsjungle@163.com>
2014-02-28 22:53:28 +0800

    indet

--- jquery-slider/slider.js
+++ jquery-slider/slider.js
@@ -1,36 +1,33 @@
 $.fn.extend({
   slider : function(){
-  	var aBtns=this.find('ol li');
-	var oUl=this.find('ul');
+    var btns=this.find('ol li');
+    var oUl=this.find('ul');
+    var currentImg=0;
+    aBtns.mouseover(function(){
+      currentImg=$(this).index();
+      tab();
+    });
 
-	var currentImg=0;
-	aBtns.mouseover(function(){
-	  currentImg=$(this).index();
-	  tab();
-	});
+    function tab(){
+      aBtns.removeClass('active');
+      aBtns.eq(currentImg).addClass('active');
+      oUl.stop().animate({top:-oUl.children().first().height()*currentImg+'px'});
+    }
 
-	function tab(){
-	  aBtns.removeClass('active');
-	  aBtns.eq(currentImg).addClass('active');
-	  oUl.stop().animate({top:-oUl.children().first().height()*currentImg+'px'});
-	}
-
-
-	function next(){
+    function next(){
 	  currentImg++;
 	  if(currentImg==aBtn.length){
   	    currentImg=0;
       }
       tab();
-	}
+    }
+    var timer;
+    timer=setInterval(next,1500);
 
-	var timer;
-	timer=setInterval(next,1500);
-		
-	this.hover(function(){
-	  clearInterval(timer);
-	},function(){
-	  timer=setInterval(next,1500)
-	});
+    this.hover(function(){
+      clearInterval(timer);
+    },function(){
+      timer=setInterval(next,1500)
+    });
   }
 });
